f99160d5aefa18aa2b60faf916af47e2c008e09f
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2022-10-16 15:00:08 -0600

    Timeblock structure created dynamically in jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,4 +41,38 @@ $(function() {
     // .saveBtn - Create onclick event to save new description to localStorage (function call)
 
 
+
+    // <div class="row timeblock">
+    // <div class="hour col-2 p-3 text-right">9AM</div>
+    // <div class="description col-9 present text-dark"> 
+    //  Current Hour
+    // </div>
+    // <div class="saveBtn col-1 d-flex justify-content-center"><i class="fas fa-save align-self-center"></i></div>
+    // </div>
+
+    var newBlock = $("<div>");
+    newBlock.addClass("row timeblock");
+    $("#timeblocks").append(newBlock);
+    
+    // Create Hour Column
+    var newBlockTime = $("<div>");
+    newBlockTime.addClass("hour col-2 p-3 text-right");
+    newBlockTime.text("9AM"); // Make dynamic in loop!
+    $(".timeblock").append(newBlockTime);
+
+    // Create Event Description Column
+    var newBlockDescription = $("<div>");
+    newBlockDescription.addClass("description col-9 p-3 present text-dark");
+    newBlockDescription.text("FPO");
+    $(".timeblock").append(newBlockDescription);
+
+    // Create Save Button Column
+    var newBlockSave = $("<div>");
+    newBlockSave.addClass("saveBtn col-1 d-flex justify-content-center");
+    $(".timeblock").append(newBlockSave);
+
+    var newBlockSaveIcon = $("<i>");
+    newBlockSaveIcon.addClass("fas fa-save align-self-center");
+    $(".saveBtn").append(newBlockSaveIcon);
+
 });
